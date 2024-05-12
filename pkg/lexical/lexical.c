1ff976122cf6dc19618255493099cfe5b851f0a3
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-05-12 20:08:15 -0300

    Added string and operator getch support.

--- pkg/lexical/lexical.c
+++ pkg/lexical/lexical.c
@@ -1,3 +1,8 @@
+/**
+ * @file lexical.c
+ * @brief Implementation of a lexical analyzer for tokenizing source code.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,12 +14,75 @@
 #define IS_ALNUM(c)  (IS_ALPHA(c) || IS_DIGIT(c))
 #define IS_WSPACE(c) ((c) == ' ' || (c) == '\t' || (c) == '\r' || (c) == '\n')
 
-Token* lexer_next_token(const char* source, const int source_size, const int cur_index) {
-    // W.I.P
+Token* lexer_next_token(const char* source, const int source_size, int cur_index) {
+    /**
+     * Function to retrieve the next token from the input source code.
+     * 
+     * @param source The input source code string.
+     * @param source_size The size of the input source code string.
+     * @param cur_index The current index in the source code string.
+     * @return A pointer to the next token.
+     */
+
+    if (cur_index >= source_size) {
+        Token* token = (Token*)malloc(sizeof(Token));
+        token->kind = TOKEN_EOF;
+        token->lexeme = NULL;
+        token->next = NULL;
+        return token;
+    }
+
+    while (IS_WSPACE(source[cur_index]))
+        cur_index++;
+
+    // Getting operators.
+    if (source[cur_index] == '+' || source[cur_index] == '-' || source[cur_index] == '*' || source[cur_index] == '/') {
+        Token* token = (Token*)malloc(sizeof(Token)); // Alloc memory necessary to initialize this Token structure.
+
+        token->kind = TOKEN_OPERATOR;
+        token->lexeme = (char*)malloc(2);
+        token->lexeme[0] = source[cur_index];
+        token->lexeme[1] = '\0';
+
+        token->next = NULL;
+        return token;
+    }
+
+    // Getting strings.
+    if (source[cur_index] == '"') {
+        int start = cur_index + 1;
+        cur_index++;
+
+        while(source[cur_index] != '"' && cur_index < source_size)
+            cur_index++;
+
+        if (source[cur_index] == '"') {
+            Token* token = (Token*)malloc(sizeof(Token)); // Alloc memory necessary to initialize this Token structure.
+            token->kind = TOKEN_STRING;
+
+            int lexeme_length = cur_index - start + 1;
+            token->lexeme = (char*)malloc(lexeme_length);
+            strncpy(token->lexeme, &source[start], lexeme_length);
+            token->lexeme[lexeme_length - 1] = '\0';
+
+            token->next = NULL;
+            return token;
+        }
+    }
+
+    return NULL;
 }
 
+
 void lexer_init(const char* source) {
-	const int source_size = strlen(source);
-	printf("%s", source);
-    printf("%d", source_size);
+    const int source_size = strlen(source);
+    int cur_index = 0;
+
+    while (cur_index < source_size) {
+        Token* token = lexer_next_token(source, source_size, cur_index);
+
+        if (token != NULL)
+            printf("Token Value: %s | Token Type: %d\n", token->lexeme, token->kind); // Show token value.
+        cur_index++;
+    }
 }
--- pkg/lexical/lexical.h
+++ pkg/lexical/lexical.h
@@ -1,23 +1,28 @@
+/**
+ * @file lexical.h
+ * @brief Defines tokens and functions for lexical analysis.
+ */
+
 #ifndef LEXICAL_H
 #define LEXICAL_H
 
 typedef enum {
-	TOKEN_NUMBER,
-	TOKEN_IDENTIFIER,
-	TOKEN_SYMBOL,
-	TOKEN_OPERATOR,
-	TOKEN_BOOLEAN,
-	TOKEN_STRING,
-	TOKEN_EOF
+    TOKEN_NUMBER,      /**< Token represents a numeric value. */
+    TOKEN_IDENTIFIER,  /**< Token represents an identifier. */
+    TOKEN_SYMBOL,      /**< Token represents a symbol. */
+    TOKEN_OPERATOR,    /**< Token represents an operator. */
+    TOKEN_BOOLEAN,     /**< Token represents a boolean value. */
+    TOKEN_STRING,      /**< Token represents a string. */
+    TOKEN_EOF          /**< Token represents end of file. */
 } TokenType;
 
 typedef struct Token {
-	TokenType kind;
-	char* lexeme;
-	struct Token* next;
+    TokenType kind;    /**< Type of the token. */
+    char* lexeme;      /**< Lexeme associated with the token. */
+    struct Token* next;/**< Pointer to the next token in a linked list. */
 } Token;
 
-void lexer_init(const char* source);
-Token* lexer_next_token(const char* source, const int source_size, const int cur_index);
+void lexer_init(const char* source); /**< Initializes the lexer with the source code. */
+Token* lexer_next_token(const char* source, const int source_size, int cur_index); /**< Retrieves the next token from the source code. */
 
 #endif
