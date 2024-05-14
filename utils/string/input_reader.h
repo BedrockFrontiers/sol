82d9137936e38882ac7b717a127bcce0a3229209
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-05-14 10:02:19 -0300

    Created string folder for utilities using string.

---
+++ utils/string/input_reader.h
@@ -0,0 +1,27 @@
+/**
+ * @file input_reader.h
+ * @brief Header file for input reading functions.
+ */
+
+#ifndef INPUT_READER_H
+#define INPUT_READER_H
+
+/**
+ * @brief Checks the input buffer for binary string characters.
+ * 
+ * This function checks the input buffer for binary string characters.
+ * 
+ * @param buffer The input buffer to be checked.
+ */
+void check_bstring_buffer(const char* buffer);
+
+/**
+ * @brief Reads input from the user.
+ * 
+ * This function reads input from the user.
+ * 
+ * @return A pointer to a character array containing the input.
+ */
+char* read_input();
+
+#endif
