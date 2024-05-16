6ab0d7420e3b233c0809907fa82f28d5f188b7f5
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-05-15 22:21:39 -0300

    Fixed operators token type and symbol catch. Added better string and comments handle and error prevent.

--- pkg/lexical/lexical.c
+++ pkg/lexical/lexical.c
@@ -7,18 +7,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
 #include "lexical.h"
 
-#define IS_ALPHA(c)  (((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) <= 'Z'))
+#define IS_ALPHA(c)  (iswalpha(c) || (c) ==  '_')
 #define IS_DIGIT(c)  ((c) >= '0' && (c) <= '9')
 #define IS_ALNUM(c)  (IS_ALPHA(c) || IS_DIGIT(c))
 #define IS_WSPACE(c) ((c) == ' ' || (c) == '\t' || (c) == '\r' || (c) == '\n')
 
+#define OPERATORS "+-*/^"
+
+int inside_string = 0;
+
+int is_operator(char c) {
+	for (int i = 0; OPERATORS[i] != '\0'; i++) {
+		if (OPERATORS[i] == c)
+			return 1;
+	}
+	return 0;
+}
+
 Token* create_token(const int cur_index, const int start, const int cur_line, const char* source) {
 	Token* token = (Token*)malloc(sizeof(Token));
+	if (token == NULL) {
+		perror("Memory allocation error");
+		exit(EXIT_FAILURE);
+	}
 
 	int lexeme_length = cur_index - start + 1;
 	token->lexeme = (char*)malloc(lexeme_length);
+	if (token->lexeme == NULL) {
+		perror("Memory allocation error");
+		exit(EXIT_FAILURE);
+	}
+
 	strncpy(token->lexeme, &source[start], lexeme_length);
 	token->lexeme[lexeme_length - 1] = '\0';
 	token->line = cur_line;
@@ -59,6 +82,9 @@ Token* lexer_next_token(const char* source, const int source_size, int* cur_inde
 		(*cur_index)++;
 	}
 
+	if (*cur_index >= source_size)
+		return NULL;
+
 	if (source[*cur_index] == '/' && *cur_index + 1 < source_size && source[*cur_index + 1] == '/') {
 		while (*cur_index < source_size && source[*cur_index] != '\n') {
 			(*cur_index)++;
@@ -80,12 +106,15 @@ Token* lexer_next_token(const char* source, const int source_size, int* cur_inde
 			(*cur_index) += 2;
 			return lexer_next_token(source, source_size, cur_index, cur_line);
 		} else {
-			Token* token = create_token(*cur_index, *cur_index, *cur_line, source);
-			return token;
+			printf("Error: Unterminated block comment at line %d\n", *cur_line);
+			getchar();
+			exit(EXIT_FAILURE);
 		}
 	}
 
 	if (source[*cur_index] == '"') {
+		inside_string = 1;
+
 		int start = (*cur_index) + 1;
 		(*cur_index)++;
 
@@ -96,12 +125,34 @@ Token* lexer_next_token(const char* source, const int source_size, int* cur_inde
 			(*cur_index)++;
 		}
 
+		if (*cur_index >= source_size) {
+		   printf("Error: Unterminated string starting at line %d\n", *cur_line);
+		   getchar();
+		   exit(EXIT_FAILURE);
+		}
+
 		if (*cur_index < source_size && source[*cur_index] == '"') {
 			Token* token = create_token(*cur_index, start, *cur_line, source);
 			token->kind = TOKEN_STRING;
+			inside_string = 0;
 			(*cur_index)++;
 			return token;
 		}
+	}
+
+	if (IS_ALNUM(source[*cur_index])) {
+		int start = *cur_index;
+		(*cur_index)++;
+
+		while (*cur_index < source_size && IS_ALNUM(source[*cur_index]))
+			(*cur_index)++;
+
+		Token* token = create_token(*cur_index, start, *cur_line, source);
+		if (strcmp(token->lexeme, "true") == 0 || strcmp(token->lexeme, "false") == 0)
+			token->kind = TOKEN_BOOLEAN;
+		else
+			token->kind = TOKEN_IDENTIFIER;
+		return token;
 	}
 
 	if (IS_DIGIT(source[*cur_index])) {
@@ -122,24 +173,9 @@ Token* lexer_next_token(const char* source, const int source_size, int* cur_inde
 		return token;
 	}
 
-	if (IS_ALPHA(source[*cur_index])) {
-		int start = *cur_index;
-		(*cur_index)++;
-
-		while (*cur_index < source_size && IS_ALPHA(source[*cur_index]))
-			(*cur_index)++;
-
-		Token* token = create_token(*cur_index, start, *cur_line, source);
-		if (strcmp(token->lexeme, "true") == 0 || strcmp(token->lexeme, "false") == 0)
-			token->kind = TOKEN_BOOLEAN;
-		else
-			token->kind = TOKEN_IDENTIFIER;
-		return token;
-	}
-
-	if (source[*cur_index] == '+' || source[*cur_index] == '-' || source[*cur_index] == '*' || source[*cur_index] == '/') {
+	if (is_operator(source[*cur_index])) {
 		Token* token = create_token(*cur_index + 1, *cur_index, *cur_line, source);
-		token->kind = TOKEN_SYMBOL;
+		token->kind = TOKEN_OPERATOR;
 		(*cur_index)++;
 		return token;
 	} else {
