26ce3339e6ed00ec8d00ed9a695b5bbf9f46250d
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-05-16 20:24:50 -0300

    Now, lexer_test2 and lexer_test is one.

--- tests/units/lexer_test.c
+++ tests/units/lexer_test.c
@@ -3,16 +3,17 @@
  * @brief Test program for the lexical analyzer.
  *
  * This program demonstrates the usage of the lexical analyzer
- * by accepting input from the user, initializing the lexer,
- * and tokenizing the input text.
+ * by accepting a text file, initializing the lexer,
+ * and tokenizing the text file.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 #include "lexical.h"
-#include "input_reader.h"
+#include "read_file.h"
 #include "directory_check.h"
 
 /**
@@ -40,19 +41,26 @@ const char* token_type_to_string(TokenType type) {
    }
 }
 
+/**
+ * @brief Main function to test the lexical analyzer.
+ * 
+ * This function reads the content of a text file, initializes
+ * the lexical analyzer, tokenizes the text file, and prints
+ * the resulting tokens to the console.
+ * 
+ * @return 0 on success, non-zero on failure.
+ */
 int main() {
    if (!verify_execution_directory("\\tests\\units\\bin")) {
       fprintf(stderr, "On running this test, run from his original path on: '\\tests\\units\\bin' to prevent errors.\n");
       getchar();
       return 1;
    }
-   char* input;
+   char* file_content;
+   file_content = read_file("..\\lexer1.txt");
 
-   printf("Insert your input here: ");
-   input = read_input();
-
-   Token* tokens = lexize(input);
-   free(input);
+   Token* tokens = lexize(file_content);
+   free(file_content);
 
    Token* current = tokens;
    while (current) {
@@ -60,6 +68,7 @@ int main() {
       current = current->next;
    }
 
+   printf("Done. Press any key to exit.");
    getchar(); // Prevent to close window on done.
    return 0;
 }
