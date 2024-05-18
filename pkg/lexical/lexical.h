6e61299c49a49f4047cf6b290fe0a6b415178aa1
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-05-17 22:38:53 -0300

    Removed 'lexer_next_token()'.

--- pkg/lexical/lexical.h
+++ pkg/lexical/lexical.h
@@ -10,24 +10,24 @@
  * @brief Represents the types of tokens used in lexical analysis.
  */
 typedef enum {
-    TOKEN_NUMBER,      /**< Token represents a numeric value. */
-    TOKEN_IDENTIFIER,  /**< Token represents an identifier. */
-    TOKEN_SYMBOL,      /**< Token represents a symbol. */
-    TOKEN_OPERATOR,    /**< Token represents an operator. */
-    TOKEN_BOOLEAN,     /**< Token represents a boolean value. */
-    TOKEN_STRING,      /**< Token represents a string. */
-    TOKEN_EOF          /**< Token represents end of file. */
+    TOKEN_NUMBER,                           /**< Token represents a numeric value. */
+    TOKEN_IDENTIFIER,                       /**< Token represents an identifier. */
+    TOKEN_SYMBOL,                           /**< Token represents a symbol. */
+    TOKEN_OPERATOR,                         /**< Token represents an operator. */
+    TOKEN_BOOLEAN,                          /**< Token represents a boolean value. */
+    TOKEN_STRING,                           /**< Token represents a string. */
+    TOKEN_EOF                               /**< Token represents end of file. */
 } TokenType;
 
 /**
  * @brief Represents a token in the lexical analysis.
  */
 typedef struct Token {
-    TokenType kind;     /**< Type of the token. */
-    char* lexeme;       /**< Lexeme associated with the token. */
-    int line;           /**< Line of token. */
-    int column;         /**< Column of token. */
-    struct Token* next; /**< Pointer to the next token in a linked list. */
+    TokenType kind;                         /**< Type of the token. */
+    char* lexeme;                           /**< Lexeme associated with the token. */
+    int line;                               /**< Line of token. */
+    int column;                             /**< Column of token. */
+    struct Token* next;                     /**< Pointer to the next token in a linked list. */
 } Token;
 
 
@@ -74,21 +74,12 @@ Token* add_token(Token* head, Token* new_token);
  * 
  * @param cur_index The current index in the source code.
  * @param start The starting index of the token.
- * @param line The line number of the token.
+ * @param cur_line The line number of the token.
+ * @param cur_column The column number of the token.
  * @param source The source code string.
+ * @param kind The token type of the token.
  * @return A pointer to the created token.
  */
-Token* create_token(const int cur_index, const int start, const int line, const int column, const char* source);
-
-/**
- * @brief Retrieves the next token from the source code.
- * 
- * @param source The source code string.
- * @param source_size The size of the source code string.
- * @param cur_index Pointer to the current index in the source code.
- * @param cur_line Pointer to the current line number.
- * @return A pointer to the next token.
- */
-Token* lexer_next_token(const char* source, const int source_size, int* cur_index, int* cur_line, int* cur_column, int* column_offset);
+Token* create_token(const int cur_index, const int start, const int cur_line, const int cur_column, const char* source, TokenType kind);
 
 #endif
