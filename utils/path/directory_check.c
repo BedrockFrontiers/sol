e1bd55311d289848d5a18d6239e9826ff98210d1
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-05-19 21:39:02 -0300

    fix: path directory path treatment, now work with "/" and "\".

--- utils/path/directory_check.c
+++ utils/path/directory_check.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include "path.h"
 
 int verify_execution_directory(const char* expected_dir) {
    char current_dir[1024];
@@ -10,13 +11,18 @@ int verify_execution_directory(const char* expected_dir) {
       return -1;
    }
 
+   normalize_path(current_dir);
+   char expected_dir_normalized[1024];
+   strcpy(expected_dir_normalized, expected_dir);
+   normalize_path(expected_dir_normalized);
+
    size_t current_dir_length = strlen(current_dir);
-   size_t expected_dir_length = strlen(expected_dir);
+   size_t expected_dir_length = strlen(expected_dir_normalized);
 
    if (current_dir_length < expected_dir_length)
       return 0;
 
-   if (strcmp(current_dir + current_dir_length - expected_dir_length, expected_dir) == 0)
+   if (strcmp(current_dir + current_dir_length - expected_dir_length, expected_dir_normalized) == 0)
       return 1;
 
    return 0;
